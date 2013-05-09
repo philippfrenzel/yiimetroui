a4525264d7fb2dd6174c4b7c9f8c93d9dc0c85e0
Philipp Frenzel <philipp@frenzel.net>
2013-05-09 10:57:27 +0200

    adding registerCoreCss to api

--- components/MuiApi.php
+++ components/MuiApi.php
@@ -12,11 +12,33 @@
 namespace vendor\yiiext\metroui\components;
 
 use Yii;
-use Yii\base\Component;
+use yii\base\Component;
+use yii\helpers\Html;
 
 /**
 * MetroUI API component.
 */
 class MuiApi extents Component{
+
+	/**
+	* @var bool whether we should copy the asset file or directory even if it is already published before.
+	*/
+	public $forceCopyAssets = false;
+
+	private $_assetsUrl;
+
+	/**
+	* Registers the MetroUI CSS.
+	* @param string $url the URL to the CSS file to register.
+	*/
+	public function registerCoreCss($url = null)
+	{
+		if ($url === null)
+		{
+			$fileName = YII_DEBUG ? 'modern.css' : 'modern.min.css';
+			$url = $this->getAssetsUrl() . '/css/' . $fileName;
+		}
+		Yii::app()->clientScript->registerCssFile($url);
+	}
 
 }
