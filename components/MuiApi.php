299d5129e7b2b572c11bb151deb686d2742ecea1
Philipp Frenzel <philipp@frenzel.net>
2013-05-09 10:50:17 +0200

    adding base api component

---
+++ components/MuiApi.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+* MuiApi class file.
+* @author Philipp Frenzel <philipp@frenzel.net>
+* @copyright Copyright &copy; Philipp Frenzel 2013-
+* @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+* @package metroui.components
+* @version 1.0.0
+*/
+
+namespace vendor\yiiext\metroui\components;
+
+use Yii;
+use Yii\base\Component;
+
+/**
+* MetroUI API component.
+*/
+class MuiApi extents Component{
+
+}
