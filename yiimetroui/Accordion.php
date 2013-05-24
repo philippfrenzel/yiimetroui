2bd6c8cde7af8cfe0ebc6c5eb5d396980437883a
Philipp Frenzel <philipp@frenzel.net>
2013-05-24 20:17:03 +0200

    refactore code to new experience

---
+++ yiimetroui/Accordion.php
@@ -0,0 +1,131 @@
+<?php
+ 
+ /**
+ * This class is merely used to publish assets that are needed by all dhtmlx
+ * widgets and thus have to be imported before any widget gets rendered.
+ * @copyright Frenzel GmbH - www.frenzel.net
+ * @link http://www.frenzel.net
+ * @author Philipp Frenzel <philipp@frenzel.net>
+ */
+
+namespace yiimetroui;
+
+use yii\base\InvalidConfigException;
+use yii\helpers\base\ArrayHelper;
+use yii\helpers\Html;
+
+/**
+ * Accordion renders an accordion bootstrap javascript component.
+ *
+ * For example:
+ *
+ * ```php
+ * echo Accordion::widget(array(
+ *     'items' => array(
+ *         // equivalent to the above
+ *         'Collapsible Group Item #1' => array(
+ *             'content' => 'Anim pariatur cliche...',
+ *             // open its content by default
+ *             'contentOptions' => array('class'=>'in')
+ *         ),
+ *         // another group item
+ *         'Collapsible Group Item #2' => array(
+ *             'content' => 'Anim pariatur cliche...',
+ *             'contentOptions' => array(...),
+ *             'options' => array(...),
+ *         ),
+ *     )
+ * ));
+ * ```
+ *
+ * @see http://twitter.github.io/bootstrap/javascript.html#Accordion
+ * @author Antonio Ramirez <amigo.cobos@gmail.com>
+ * @since 2.0
+ */
+class Accordion extends Widget
+{
+	/**
+	 * @var array list of groups in the Accordion widget. Each array element represents a single
+	 * group with the following structure:
+	 *
+	 * ```php
+	 * // item key is the actual group header
+	 * 'Collapsible Group Item #1' => array(
+	 *     // required, the content (HTML) of the group
+	 *     'content' => 'Anim pariatur cliche...',
+	 *     // optional the HTML attributes of the content group
+	 *     'contentOptions'=> array(),
+	 *     // optional the HTML attributes of the group
+	 *     'options'=> array(),
+	 * )
+	 * ```
+	 */
+	public $items = array();
+
+
+	/**
+	 * Initializes the widget.
+	 */
+	public function init()
+	{
+		parent::init();		
+		$this->addCssClass(ArrayHelper::merge($this->options,array('data-role'=>'accordion')), 'accordion');
+	}
+
+	/**
+	 * Renders the widget.
+	 */
+	public function run()
+	{
+		echo Html::beginTag('div', $this->options) . "\n";
+		echo $this->renderItems() . "\n";
+		echo Html::endTag('div') . "\n";
+	}
+
+	/**
+	 * Renders collapsible items as specified on [[items]].
+	 * @return string the rendering result
+	 */
+	public function renderItems()
+	{
+		$items = array();
+		$index = 0;
+		foreach ($this->items as $header => $item) {
+			$options = ArrayHelper::getValue($item, 'options', array());
+			//$this->addCssClass($options, 'accordion-group');
+			$items[] = Html::tag('li', $this->renderItem($header, $item, ++$index), $options);
+		}
+
+		return implode("\n", $items);
+	}
+
+	/**
+	 * Renders a single collapsible item group
+	 * @param string $header a label of the item group [[items]]
+	 * @param array $item a single item from [[items]]
+	 * @param integer $index the item index as each item group content must have an id
+	 * @return string the rendering result
+	 * @throws InvalidConfigException
+	 */
+	public function renderItem($header, $item, $index)
+	{
+		if (isset($item['content'])) {
+			$id = $this->options['id'] . '-Accordion' . $index;
+			$options = ArrayHelper::getValue($item, 'contentOptions', array());
+			$options['id'] = $id;
+			$this->addCssClass($options, 'accordion-body Accordion');
+
+			$header = Html::a($header, '#' . $id, array()) . "\n";
+
+			$content = Html::tag('div', $item['content'], array()) . "\n";
+		} else {
+			throw new InvalidConfigException('The "content" option is required.');
+		}
+		$group = array();
+
+		$group[] = $header;
+		$group[] = Html::tag('div', $content, $options);
+
+		return implode("\n", $group);
+	}
+}
---
+++ yiimetroui/Widget.php
@@ -0,0 +1,96 @@
+<?php
+ 
+ /**
+ * This class is merely used to publish assets that are needed by all dhtmlx
+ * widgets and thus have to be imported before any widget gets rendered.
+ * @copyright Frenzel GmbH - www.frenzel.net
+ * @link http://www.frenzel.net
+ * @author Philipp Frenzel <philipp@frenzel.net>
+ */
+
+namespace yiimetroui;
+
+use Yii;
+use yii\base\View;
+use yii\helpers\Json;
+
+class Widget extends yii\base\Widget
+{
+	/**
+	 * @var string the dhtmlx theme bundle.
+	 */
+	//public static $theme = 'yiiext/dhtmlx/theme/dhx_web';
+	/**
+	 * @var array the HTML attributes for the widget container tag.
+	 */
+	public $options = array();
+	/**
+	 * @var array the options for the underlying jQuery UI widget.
+	 * Please refer to the corresponding jQuery UI widget Web page for possible options.
+	 * For example, [this page](http://api.jqueryui.com/accordion/) shows
+	 * how to use the "Accordion" widget and the supported options (e.g. "header").
+	 */
+	public $clientOptions = array();
+	/**
+	 * @var array the event handlers for the underlying jQuery UI widget.
+	 * Please refer to the corresponding jQuery UI widget Web page for possible events.
+	 * For example, [this page](http://api.jqueryui.com/accordion/) shows
+	 * how to use the "Accordion" widget and the supported events (e.g. "create").
+	 */
+	public $clientEvents = array();
+
+
+	/**
+	 * Initializes the widget.
+	 * If you override this method, make sure you call the parent implementation first.
+	 */
+	public function init()
+	{
+		parent::init();
+		$this->registerPlugin(strtolower(get_called_class())); 
+		if (!isset($this->options['id'])) {
+			$this->options['id'] = $this->getId();
+		}
+	}
+
+	/**
+	* Registers a specific Bootstrap plugin and the related events
+	* @param string $name the name of the Bootstrap plugin
+	*/
+	protected function registerPlugin($name)
+	{
+		$id = $this->options['id'];
+		$view = $this->getView();
+		
+		if ($this->clientOptions !== false) {
+			$options = empty($this->clientOptions) ? '' : Json::encode($this->clientOptions);
+			$js = "jQuery('#$id').$name($options);";
+			$view->registerJs($js);
+		}
+
+		if (!empty($this->clientEvents)) {
+			$js = array();
+			foreach ($this->clientEvents as $event => $handler) {
+				$js[] = "jQuery('#$id').on('$event', $handler);";
+			}
+			$view->registerJs(implode("\n", $js));
+		}
+	}
+
+	/**
+	* Adds a CSS class to the specified options.
+	* This method will ensure that the CSS class is unique and the "class" option is properly formatted.
+	* @param array $options the options to be modified.
+	* @param string $class the CSS class to be added
+	*/
+	protected function addCssClass(&$options, $class)
+	{
+		if (isset($options['class'])) {
+			$classes = preg_split('/\s+/', $options['class'] . ' ' . $class, -1, PREG_SPLIT_NO_EMPTY);
+			$options['class'] = implode(' ', array_unique($classes));
+		} else {
+			$options['class'] = $class;
+		}
+	}
+
+}
